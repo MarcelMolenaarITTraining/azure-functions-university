82d53d4b290f88d235ab618a76f6a295d4701979
Marc Duiker <marcduiker@gmail.com>
2021-06-02 12:05:11 +0200

    Start with NET5 content

---
+++ src/dotnet5/AzureFunctions.Http/HelloWorldHttpTrigger.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunctionsUniversity.Demo
+{
+    public static class HelloWorldHttpTrigger
+    {
+        [Function("HelloWorldHttpTrigger")]
+        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger("HelloWorldHttpTrigger");
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+        }
+    }
+}
